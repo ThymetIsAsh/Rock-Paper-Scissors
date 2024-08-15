e6da902731879240ebc5f22b8ba592fb6365d1e5
Bhuvaneshwaran P <logeshbhuvanesh2123@gmail.com>
2024-08-16 00:09:09 +0530

    Add Functions

--- javs.js
+++ javs.js
@@ -4,15 +4,15 @@ function getComputerChoice() {
     let result = Math.floor(Math.random() * (4 - 2 + 1) + 2);
     let answer;
     if (result === 2) {
-    answer = "rock";
-} else if (result === 3) {
-    answer = "paper";
-} else if (result === 4) {
-    answer = "scissor";
-} else {
-    alert("Something Wrong with this code!");
-}
-return answer;
+        answer = "rock";
+    } else if (result === 3) {
+        answer = "paper";
+    } else if (result === 4) {
+        answer = "scissor";
+    } else {
+        alert("Something Wrong with this code!");
+    }
+    return answer;
 }
 
 function getHumanChoice() {
@@ -21,21 +21,35 @@ function getHumanChoice() {
 
 function playRound(humanChoice, computerChoice) {
     // your code here!
-if 
+    if (humanChoice === "rock" && computerChoice === "scissor" ||
+        humanChoice === "paper" && computerChoice === "rock" ||
+        humanChoice === "scissor" && computerChoice === "paper") {
+        console.log("You win! " + humanChoice + " beats " + computerChoice);
+        humanScore++;
+    } else if (humanChoice === computerChoice) {
+        console.log("Tie..You Both Choose" + humanChoice)
+    } else {
+        console.log("You Loss! " + computerChoice + " beats " + humanChoice);
+        computerScore++;
+    }
+}
 
-  }
+function playGame(){
 
-  
-  let answer,humanScore = 0, computerScore = 0;
-  const humanSelection = getHumanChoice();
-  const computerSelection = getComputerChoice();
-
-  console.log(humanSelection);
-
-  
-  
-  
-  
+}
 
 
- 
+let answer, humanScore = 0, computerScore = 0;
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+playRound(humanSelection, computerSelection);
+
+console.log(humanScore+" "+computerScore);
+
+
+
+
+
+
+
+
